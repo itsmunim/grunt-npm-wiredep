327c9038d5d899fe2d97e6366733b3d7e27eafcd
dibosh <md-abdul_munim@astro.com.my>
2019-08-29 15:38:00 +0800

    Add npmWiredep task

---
+++ tasks/npm_wiredep.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * Inspired by https://github.com/stephenplusplus/grunt-wiredep
+ * Similar kind of task; only for npm dependencies. Helps when you
+ * want to convert your bower dependent project into a npm one.
+ * @param {*} grunt
+ */
+function npmWiredep(grunt) {
+  grunt.registerMultiTask('npmWiredep', 'Inject npm packages into your source code', function () {
+    this.requiresConfig(['wiredep', this.target, 'src']);
+
+    var options = this.options(this.data);
+    require('npm-wiredep')(options);
+  });
+}
+
+module.exports = npmWiredep;
